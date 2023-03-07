f0dea0fbbe844541bfd4169fbb0f37863a84655f
Alf Delgado <github@delhelsa.com>
2023-03-06 19:26:57 -0600

    Archivos de ejemplo iniciales para la cuarta clase.

---
+++ 04/01-personas.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# coding: utf-8
+personas = []
+pactual = {}
+print("Por favor ingrese los datos de una persona a la vez.\nDeje el nombre en blanco cuando no desee ingresar más personas.")
+pactual["nombre"]=input("Nombre: ")
+pactual["apellido"]=input("Apellido: ")
+pactual["cedula"]=input("Cédula: ")
+pactual["edad"]=int(input("Edad: "))
+personas.append(pactual)
+while pactual["nombre"] != "" :
+    pactual["nombre"]=input("Nombre: ")
+    pactual["apellido"]=input("Apellido: ")
+    pactual["cedula"]=input("Cédula: ")
+    pactual["edad"]=int(input("Edad: "))
+    personas.append(pactual)
+k=1
+for persona in personas :
+    print("La persona número",k,"es",persona["nombre"],persona["apellido"]+".\nTiene",persona["edad"],"años, y su cédula es",persona["cedula"]+".\n")
+    k+=1
+print("Fin del programa.")
